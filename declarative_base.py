547e3ca10298edc69bd317834b1f0d97e28254e8
fransindi <frangarciasamat@gmail.com>
2024-01-08 12:32:16 -0300

    primer commit

---
+++ declarative_base.py
@@ -0,0 +1,14 @@
+# Importamos 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Creamos el motor con la direccion de la base de datos
+                             #   ↓ aqui agregamos la direccion
+engine = create_engine('sqlite:///baseDeDatos.sqlite')
+
+# Creamos la sesion que interactua con el motor
+session = sessionmaker(bind=engine)
+
+# Creamos la clase Base la cual hereda las clases del diagramaS
+Base = declarative_base()
